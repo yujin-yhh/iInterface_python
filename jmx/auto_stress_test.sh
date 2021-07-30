d223adb9d40369c02bd3e3f573b8ee55f47e253e
yujin-yhh <68677295+yujin-yhh@users.noreply.github.com>
2021-07-30 17:51:05 +0800

    Update auto_stress_test.sh

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -7,7 +7,7 @@ export jmx_template_filename="${jmx_template}${suffix}"
 export os_type=`uname`
 
 # 需要在系统变量中定义jmeter根目录的位置，如下
-export jmeter_path="/usr/local/apache-jmeter-5.4.1/bin/jmeter"
+export jmeter_path="/usr/local/apache-jmeter-5.4.1"
 
 echo "自动化压测开始"
 
